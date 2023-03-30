3b92f932dd5df20b9bf9103f7bb16135d3d35ad0
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-03-30 19:19:50 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,33 +1,31 @@
-   // Maps numbers to colors using an object
-   const colorMap = {
-    1: "red",
-    2: "blue",
-    3: "green",
-    4: "yellow",
-    5: "orange",
-    6: "purple",
-  };
+const colors = ["red", "blue", "green", "yellow", "orange", "purple"];
 
-  function mapToColor(num) {
-    return colorMap[num];
-  }
+function rollDice() {
+  const diceRolls = rollThreeDice();
+  const resultEl = document.getElementById("result");
+  resultEl.innerHTML =
+    "You rolled: <br>" +
+    diceRolls
+      .join(" ")
+      .replace(
+        /(red|blue|green|yellow|orange|purple)/g,
+        '<span style="color: $1">$1</span>'
+      );
+}
 
-  function rollDice() {
-    const diceRolls = rollThreeDice();
-    const resultEl = document.getElementById("result");
-    resultEl.innerHTML = "You rolled: <br>" +
-      diceRolls
-        .join(" ")
-        .replace(
-          /(red|blue|green|yellow|orange|purple)/g,
-          '<span style="color: $1">$1</span>'
-        );
+function rollThreeDice() {
+  const shuffledColors = shuffle(colors);
+  const diceRolls = [];
+  for (let i = 0; i < 3; i++) {
+    diceRolls.push(shuffledColors[i]);
   }
+  return diceRolls;
+}
 
-  function rollThreeDice() {
-    const diceRolls = [];
-    for (let i = 0; i < 3; i++) {
-      diceRolls.push(mapToColor(Math.floor(Math.random() * 6) + 1));
-    }
-    return diceRolls;
+function shuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [array[i], array[j]] = [array[j], array[i]];
   }
+  return array;
+}
