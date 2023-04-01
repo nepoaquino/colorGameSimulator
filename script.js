002a507b4152617281f37c9ce6b8f814b8d4adc6
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-04-01 22:21:24 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
 "use strict";
 
 const colors = ["red", "blue", "green", "yellow", "orange", "purple"];
+const colorSvgs = {
+  red: '<svg width="50" height="50"><rect width="50" height="50" style="fill:red"/></svg>',
+  blue: '<svg width="50" height="50"><rect width="50" height="50" style="fill:blue"/></svg>',
+  green: '<svg width="50" height="50"><rect width="50" height="50" style="fill:green"/></svg>',
+  yellow: '<svg width="50" height="50"><rect width="50" height="50" style="fill:yellow"/></svg>',
+  orange: '<svg width="50" height="50"><rect width="50" height="50" style="fill:orange"/></svg>',
+  purple: '<svg width="50" height="50"><rect width="50" height="50" style="fill:purple"/></svg>'
+};
 const history = [];
 
 function rollDice() {
-  const [color1, color2, color3] = shuffle(colors).slice(0, 3);
+  const resultColors = [];
+  for (let i = 0; i < 3; i++) {
+    const randomIndex = Math.floor(Math.random() * colors.length);
+    resultColors.push(colors[randomIndex]);
+  }
+
   const resultElement = document.getElementById("result");
-  const resultHTML = [color1, color2, color3]
-    .map((color) => `<span style="color: ${color}">${color.toUpperCase()}</span>`)
-    .join(" ");
+  const resultHTML = resultColors
+    .map((color) => colorSvgs[color])
+    .join("");
   resultElement.innerHTML = `You rolled: <br>${resultHTML}`;
 
   // add the result to the history if it doesn't already exist
-  const resultText = [color1, color2, color3].join(" ");
+  const resultText = resultColors.join(" ");
   if (!history.some((item) => item.text === resultText)) {
     history.push({
       text: resultText,
