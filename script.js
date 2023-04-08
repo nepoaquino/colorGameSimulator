4e917cc9c4548ec53b3c8a106ab6ec76bc4e3cf3
Nepo Aquino <124418583+neppy3250@users.noreply.github.com>
2023-04-08 19:46:40 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,49 +1,52 @@
 "use strict";
 
-const colors = ["red", "blue", "green", "yellow", "orange", "purple"];
-const colorSvgs = {
-  red: '<svg width="50" height="50"><rect width="50" height="50" style="fill:red"/></svg>',
-  blue: '<svg width="50" height="50"><rect width="50" height="50" style="fill:blue"/></svg>',
-  green: '<svg width="50" height="50"><rect width="50" height="50" style="fill:green"/></svg>',
-  yellow: '<svg width="50" height="50"><rect width="50" height="50" style="fill:yellow"/></svg>',
-  orange: '<svg width="50" height="50"><rect width="50" height="50" style="fill:orange"/></svg>',
-  purple: '<svg width="50" height="50"><rect width="50" height="50" style="fill:purple"/></svg>'
-};
+const colors = ["red", "blue", "green", "yellow", "#ddd", "purple"];
+
+const colorSvgs = Object.fromEntries(
+  colors.map((color) => [
+    color,
+    `<svg><rect width="100" height="100" style="fill:${color}"/></svg>`,
+  ])
+);
+
 const history = [];
 
 function rollDice() {
-  const resultColors = [];
-  for (let i = 0; i < 3; i++) {
-    const randomIndex = Math.floor(Math.random() * colors.length);
-    resultColors.push(colors[randomIndex]);
-  }
+  const shuffledColors = shuffle(colors);
+  const [color1, color2, color3] = shuffledColors.slice(0, 3);
+  const resultColors = [color1, color2, color3];
 
   const resultElement = document.getElementById("result");
-  const resultHTML = resultColors
-    .map((color) => colorSvgs[color])
-    .join("");
+  const resultHTML = resultColors.map((color) => colorSvgs[color]).join("");
   resultElement.innerHTML = `You rolled: <br>${resultHTML}`;
 
-  // add the result to the history if it doesn't already exist
+  // add the result to the history
   const resultText = resultColors.join(" ");
-  if (!history.some((item) => item.text === resultText)) {
-    history.push({
-      text: resultText,
-      html: resultHTML,
-    });
-  }
+  history.unshift({
+    text: resultText,
+    html: resultHTML,
+  });
+
+  // update the history table
+  const historyTable = document.getElementById("history-table");
+  const newRow = document.createElement("tr");
+  newRow.innerHTML = `
+    <td>${history.length}</td>
+    <td>${resultHTML}</td>
+  `;
+  const tbody = historyTable.querySelector("tbody");
+  tbody.insertBefore(newRow, tbody.firstChild);
 }
 
 function toggleHistory() {
   const historyElement = document.getElementById("history");
-  const historyTable = document.getElementById("history-table") || document.createElement("table");
-  historyTable.setAttribute("id", "history-table");
+  const historyTable = document.getElementById("history-table");
 
   if (historyElement.style.display === "none") {
     // show the history
     historyElement.style.display = "block";
 
-    // add each unique item in the history to the table
+    // add each item in the history to the table
     historyTable.innerHTML = `
       <thead>
         <tr>
@@ -52,18 +55,16 @@ function toggleHistory() {
         </tr>
       </thead>
       <tbody>
-        ${history.map((item, index) => {
-          if (index === 0 || item.text !== history[index - 1].text) {
+        ${history
+          .map((item, index) => {
             return `
-              <tr>
-                <td>${index + 1}</td>
-                <td>${item.html}</td>
-              </tr>
-            `;
-          } else {
-            return "";
-          }
-        }).join("")}
+          <tr>
+            <td>${history.length - index}</td>
+            <td>${item.html}</td>
+          </tr>
+        `;
+          })
+          .join("")}
       </tbody>
     `;
 
